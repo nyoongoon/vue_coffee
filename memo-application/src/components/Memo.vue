d72bca7c2ab57f97b2ebffb0012ec6ad206821af
nyoongoon <nyoongoon@naver.com>
2023-04-17 19:26:50 +0900

    메모 삭제 기능 추가

--- memo-application/src/components/Memo.vue
+++ memo-application/src/components/Memo.vue
@@ -1,13 +1,63 @@
 <template>
-
+  <li class="memo-item">
+    <strong>{{ memo.title }}</strong>
+    <p>{{ memo.content }}</p>
+    <button type="button" @click="deleteMemo">
+      <i class="fas fa-times">X</i>
+    </button>
+  </li>
 </template>
 
 <script>
 export default {
-  name: "Memo"
+  name: "Memo",
+  props: {
+    memo: {
+      type: Object
+    }
+  },
+  methods: {
+    deleteMemo() {
+      const id = this.memo.id;
+      this.$emit('deleteMemo', id);
+    }
+  }
 }
 </script>
 
 <style scoped>
+.memo-item {
+  overflow: hidden;
+  position: relative;
+  margin-bottom: 20px;
+  paddinf: 24px;
+  box-shadow: 0 4px 10px -4px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  list-style: none;
+}
 
+.memo-item button {
+  background: none;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  font-size: 20px;
+  color: #e5e5e5;
+  border: 0;
+}
+
+.memo-item strong {
+  display: block;
+  margin-bottom: 12px;
+  font-size: 18px;
+  font-weight: normal;
+  word-break: break-all;
+}
+
+.memo-item p {
+  margin: 0;
+  font-size: 14px;
+  line-height: 22px;
+  color: #666;
+}
 </style>
--- memo-application/src/components/MemoApp.vue
+++ memo-application/src/components/MemoApp.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="memo-app">
-<!--    <memo-form v-on:addMemo="addMemo"/> 아래와 같은 의미 -->
+    <!--    <memo-form v-on:addMemo="addMemo"/> 아래와 같은 의미 -->
     <memo-form @addMemo="addMemo"/>
-    <memo/>
+    <ul class="memo-list">
+      <memo v-for="memo in memos"
+            :key="memo.id"
+            :memo="memo"
+            @deleteMemo ="deleteMemo" />
+    </ul>
   </div>
 </template>
 
@@ -21,16 +26,21 @@ export default {
     this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
   },
   components: {
-      MemoForm, Memo
+    MemoForm, Memo
   },
   methods: {
-    addMemo(payload){
+    deleteMemo(id){
+      const targetIndex = this.memos.findIndex(v => v.id === id);
+      this.memos.splice(targetIndex, 1);
+      this.storeMemo();
+    },
+    addMemo(payload) {
       // MemoForm에서 올려받은 데이터를 먼저 컴포넌트 내부데이터에 추가
       this.memos.push(payload);
       // 내부 데이터를 문자열로 변환 후, 로컬 스토리지에 저장
       this.storeMemo();
     },
-    storeMemo(){
+    storeMemo() {
       const memosToString = JSON.stringify(this.memos);
       localStorage.setItem('memos', memosToString);
     }
@@ -39,5 +49,8 @@ export default {
 </script>
 
 <style scoped>
-
+.memo-list {
+  padding: 20px 0;
+  margin: 0;
+}
 </style>
