37cfabd465d5c3edfa3f82f4e832bd340cfa4ea1
yoon <nyoongoon@naver.com>
2023-05-08 07:54:43 +0900

    signin 컴포넌트 작업 중

--- board-example-frontend/src/pages/Signup.vue
+++ board-example-frontend/src/pages/Signup.vue
@@ -2,19 +2,33 @@
   <div class="sign-up-page">
     <h1>회원가입</h1>
     <signup-form @submit="onSubmit"/>
+    <p>
+      이미 가입하셨나요?
+      <router-link to="{name: 'Signup'}">로그인하러 가기</router-link>
+    </p>
   </div>
-  <p>
-    이미 가입하셨나요?
-    <router-link to="{name: 'Signup'}">로그인하러 가기</router-link>
-  </p>
 </template>
 
 <script>
 import SignupForm from "../components/SignupForm.vue";
+import api from "@/api";
 
 export default {
   name: "Signup",
-  components: {SignupForm}
+  components: {SignupForm},
+  methods: {
+    onSubmit(payload) {
+      const {email, password, name} = payload;
+      api.post('/auth/signup', {name, email, password})
+        .then(res => {
+          alert('회원가입이 완료되었습니다.');
+          this.$router.push('/');
+        })
+        .catch(err => {
+          alert(err.response.data.msg)
+        })
+    }
+  }
 }
 </script>
 
--- board-example-frontend/src/router/index.js
+++ board-example-frontend/src/router/index.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 import PostListPage from "@/pages/PostListPage.vue";
 import PostViewPage from "@/pages/PostViewPage.vue";
 import Signup from "@/pages/Signup.vue";
+import Signin from "@/pages/Signin.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -25,6 +26,11 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
   ]
-})
+});
